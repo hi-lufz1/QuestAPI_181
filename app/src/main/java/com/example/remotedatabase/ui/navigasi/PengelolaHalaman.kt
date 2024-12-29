815f496dc14454f4c8f63233a221f245b1fc78e1
hi-lufz1 <latifufauzi@gmail.com>
2024-12-30 00:44:11 +0700

    membuat pengelola halaman

---
+++ app/src/main/java/com/example/remotedatabase/ui/navigasi/PengelolaHalaman.kt
@@ -0,0 +1,46 @@
+package com.example.remotedatabase.ui.navigasi
+
+import android.os.Build
+import androidx.annotation.RequiresExtension
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.remotedatabase.ui.view.DestinasiEntry
+import com.example.remotedatabase.ui.view.DestinasiHome
+import com.example.remotedatabase.ui.view.EntryMhsScreen
+import com.example.remotedatabase.ui.view.HomeScreen
+
+@RequiresExtension(extension = Build.VERSION_CODES.S, version = 7)
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier = Modifier,
+    navController: NavHostController = rememberNavController()
+){
+    NavHost (
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier,
+    ){
+        composable(DestinasiHome.route){
+            HomeScreen(
+                navigateToItemEntry = {navController.navigate(DestinasiEntry.route)},
+                onDetailClick = {
+
+
+                }
+            )
+        }
+        composable(DestinasiEntry.route){
+            EntryMhsScreen(navigateBack = {
+                navController.navigate(DestinasiHome.route){
+                    popUpTo(DestinasiHome.route){
+                        inclusive = true
+                    }
+                }
+            })
+        }
+    }
+}
