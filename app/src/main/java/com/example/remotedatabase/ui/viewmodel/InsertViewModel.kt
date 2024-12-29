c266b5b303d05540fc6b8ef4fc907717b696c9f9
hi-lufz1 <latifufauzi@gmail.com>
2024-12-29 20:44:22 +0700

    membuat view model insert

--- app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/remotedatabase/ui/viewmodel/InsertViewModel.kt
@@ -1,9 +1,34 @@
 package com.example.remotedatabase.ui.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.remotedatabase.model.Mahasiswa
+import com.example.remotedatabase.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
 
+class InsertViewModel(private val mhs: MahasiswaRepository) : ViewModel() {
+    var uiState by mutableStateOf(InsertUiState())
+        private set
 
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent) {
+        uiState = InsertUiState(insertUIEvent = insertUiEvent)
+    }
+
+
+    suspend fun insertMhs() {
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUIEvent.toMhs())
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+    }
+}
 
 
 data class InsertUiState(
